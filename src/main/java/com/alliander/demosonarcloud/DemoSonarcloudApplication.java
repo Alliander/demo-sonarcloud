610338de1473e164ea5a51ac932bc140c9c56f4d
mustaphaz <mustapha.zeamari@alliander.com>
2018-04-30 08:23:07 +0200

    first commit

---
+++ src/main/java/com/alliander/demosonarcloud/DemoSonarcloudApplication.java
@@ -0,0 +1,12 @@
+package com.alliander.demosonarcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSonarcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSonarcloudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alliander/demosonarcloud/DemoSonarcloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alliander.demosonarcloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSonarcloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
